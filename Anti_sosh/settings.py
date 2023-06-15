e1a514ba34c0d1d94e9b808858e3ff6eba6744cb
user <jojijose101@gmail.com>
2023-06-15 20:21:04 +0530

    third commit

--- Anti_sosh/settings.py
+++ Anti_sosh/settings.py
@@ -142,4 +142,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT =  '/media/'
